a8d72a196560cecbd6102a0d506f156d8212a3f9
Cj Fu <105927887@student.swin.edu.au>
2025-05-08 13:40:35 +1000

    added logic p2

--- cars.php
+++ cars.php
@@ -5,8 +5,19 @@
 
         $query = "SELECT * FROM cars";
         $result = mysqli_query($dbconn, $query);
-        if ($result){"It works"}
-        else {"It doesn't"}
+
+        if ($result) {
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "Car ID: " . $row['id'] . " - Name: " . $row['name'] . "<br>";
+                }
+            } else {
+                echo "<p>No cars found in the database.</p>";
+            }
+        } else {
+            echo "<p>Error executing query: " . mysqli_error($dbconn) . "</p>";
+        }
+
         mysqli_close($dbconn);
     } else echo "<p>Unable to connect to the db.</p>";
 ?>
