88312d0725dc3705afa8a4dd5b459ca8b5c7daae
Cj Fu <105927887@student.swin.edu.au>
2025-05-08 13:21:31 +1000

    renamed a file

--- cars.php
+++ cars.php
@@ -1,15 +1,15 @@
 <?php
-require_once "setting.php"
-$dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
-if ($dbconn) {
+    require_once "setting.php"
+    $dbconn = @mysqli_connect($host, $user, $pwd, $sql_db);
+    if ($dbconn) {
 
-    $query = "SELECT * FROM cars";
-    $result = mysqli_query($dbconn, $query);
-    if ($result){...}
-    else {...}
-    ...
-    mysqli_close($dbconn);
-} else echo "<p>Unable to connect to the db.</p>";
+        $query = "SELECT * FROM cars";
+        $result = mysqli_query($dbconn, $query);
+        if ($result){...}
+        else {...}
+        ...
+        mysqli_close($dbconn);
+    } else echo "<p>Unable to connect to the db.</p>";
 ?>
 
 <?php
