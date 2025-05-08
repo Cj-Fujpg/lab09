0ba762e44ed4349e3246e6afa536788102c793b6
Cj Fu <105927887@student.swin.edu.au>
2025-05-08 13:54:39 +1000

    uploaded the database

--- settings.php
+++ settings.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $username = "localhost";
 $password = "";
-$database = "exhibition_db"; 
+$database = "exhibition_db.sql"; 
 
-$dbconn = mysqli_connect($host, $database);
+$dbconn = mysqli_connect($host, $username, $password, $database);
 
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
