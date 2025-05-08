b4a511e38ac756940d0330377cf144d6b9ed1c90
Cj Fu <105927887@student.swin.edu.au>
2025-05-08 13:51:33 +1000

    testing agian

--- settings.php
+++ settings.php
@@ -4,7 +4,7 @@ $username = "localhost";
 $password = "";
 $database = "exhibition_db"; 
 
-$dbconn = mysqli_connect($host, $username, $password, $database);
+$dbconn = mysqli_connect($host, $database);
 
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
